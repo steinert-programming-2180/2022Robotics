9edd6aa4dec9aba6753e5376e129a8ef5d84c833
ZG Design Laptop <steinertrobotics@gmail.com>
2022-02-12 14:34:24 -0500

    Disables Lights when activated

---
+++ src/main/java/frc/robot/commands/limelight/DisableLights.java
@@ -0,0 +1,15 @@
+package frc.robot.commands.limelight;
+
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class DisableLights extends CommandBase{
+
+    public DisableLights() {}
+
+    @Override
+    public void initialize() {
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1);
+    }
+
+}
