fe87c1ff2ce45aa254b649ff0876e4f065209b9e
ZG Design Laptop <steinertrobotics@gmail.com>
2022-02-12 14:35:14 -0500

    Periodic Command ran when LimeLight active

---
+++ src/main/java/frc/robot/commands/limelight/LimelightPeriodic.java
@@ -0,0 +1,22 @@
+package frc.robot.commands.limelight;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Limelight;
+
+public class LimelightPeriodic extends CommandBase{
+    private final Limelight m_limelight;
+
+    public LimelightPeriodic(Limelight lime) {
+        m_limelight = lime;
+
+        addRequirements(m_limelight);
+
+    }
+
+    @Override
+    public void execute() {
+        m_limelight.putItems();
+    }
+
+
+}
