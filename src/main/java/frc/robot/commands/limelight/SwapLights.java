edacd3927a72ec0b9fe7223ea6159af9deb84937
ZG Design Laptop <steinertrobotics@gmail.com>
2022-02-16 20:34:45 -0500

    New command to swap LED status on a joystick

---
+++ src/main/java/frc/robot/commands/limelight/SwapLights.java
@@ -0,0 +1,25 @@
+package frc.robot.commands.limelight;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class SwapLights extends CommandBase{
+
+    NetworkTableEntry ledMode;
+
+    public SwapLights() {
+        ledMode = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode");
+    }
+
+    @Override
+    public void initialize() {
+        double currentLedMode = (double) ledMode.getNumber(0);
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1 - currentLedMode);
+    }
+
+    
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+}
