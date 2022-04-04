daa0d9bd0301b612a3c6de7df42d3ac3b1b36622
TJ O <tjomiatek@gmail.com>
2022-04-04 14:30:56 -0400

    Changed to use GenericHID to use XBoxController

--- src/main/java/frc/robot/utils/AxisTrigger.java
+++ src/main/java/frc/robot/utils/AxisTrigger.java
@@ -1,23 +1,22 @@
 package frc.robot.utils;
 import edu.wpi.first.wpilibj2.command.button.Button;
-
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.GenericHID;
 
 public class AxisTrigger extends Button {
 
-    Joystick joystick;
+    GenericHID joystick;
     int axis;
     double threshold;
     boolean lessThan;
 
-    public AxisTrigger(Joystick joystick, int axis, double threshold, boolean lessThan) {
+    public AxisTrigger(GenericHID joystick, int axis, double threshold, boolean lessThan) {
         this.joystick = joystick;
         this.axis = axis;
         this.threshold = threshold;
         this.lessThan = lessThan;
     }
 
-    public AxisTrigger(Joystick joystick, int axis, double threshold) {
+    public AxisTrigger(GenericHID joystick, int axis, double threshold) {
         this(joystick, axis, threshold, false);
     }
 
